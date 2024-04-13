07ac457a40cbf43d8ed20107bac75c468811d062
Inverter Team <154754910+DanilVusenko86@users.noreply.github.com>
2024-04-13 10:23:23 +0300

    Add files via upload

---
+++ user.js
@@ -0,0 +1,31 @@
+pref('geo.gps.supl_server', "supl.izatcloud.net");
+pref('geo.gps.supl_port', 22024);
+pref('dom.payment.provider.0.name', "firefoxmarket");
+pref('dom.payment.provider.0.description', "marketplace.firefox.com");
+pref('dom.payment.provider.0.uri', "https://marketplace.firefox.com/mozpay/?req=");
+pref('dom.payment.provider.0.type', "mozilla/payments/pay/v1");
+pref('dom.payment.provider.0.requestMethod', "GET");
+user_pref('b2g.system_manifest_url', "app://system.gaiamobile.org/manifest.webapp");
+user_pref('b2g.neterror.url', "app://system.gaiamobile.org/net_error.html");
+user_pref('b2g.system_startup_url', "app://system.gaiamobile.org/index.html");
+user_pref('network.http.max-connections-per-server', 15);
+user_pref('dom.mozInputMethod.enabled', true);
+user_pref('layout.css.scroll-behavior.enabled', true);
+user_pref('layout.css.sticky.enabled', true);
+user_pref('intl.uidirection.ar-x-psbidi', "rtl");
+user_pref('ril.debugging.enabled', false);
+user_pref('dom.mms.version', 17);
+user_pref('b2g.wifi.allow_unsafe_wpa_eap', true);
+user_pref('network.dns.localDomains', "gaiamobile.org,theme.gaiamobile.org,clock.gaiamobile.org,marketplace.firefox.com.gaiamobile.org,callscreen.gaiamobile.org,communications.gaiamobile.org,search.gaiamobile.org,network-alerts.gaiamobile.org,settings.gaiamobile.org,bookmark.gaiamobile.org,bluetooth.gaiamobile.org,video.gaiamobile.org,calendar.gaiamobile.org,operatorvariant.gaiamobile.org,homescreen.gaiamobile.org,wallpaper.gaiamobile.org,gallery.gaiamobile.org,keyboard.gaiamobile.org,costcontrol.gaiamobile.org,fl.gaiamobile.org,emergency-call.gaiamobile.org,findmydevice.gaiamobile.org,wappush.gaiamobile.org,sms.gaiamobile.org,email.gaiamobile.org,default_theme.gaiamobile.org,music.gaiamobile.org,sync.gaiamobile.org,verticalhome.gaiamobile.org,ringtones.gaiamobile.org,system.gaiamobile.org,camera.gaiamobile.org,ftu.gaiamobile.org,sharedtest.gaiamobile.org,fm.gaiamobile.org,pdfjs.gaiamobile.org,download.gaiamobile.org,in_app_pay_test.gaiamobile.org,nfc-api-test.gaiamobile.org,theme-test-1.gaiamobile.org,mobile-wallet.gaiamobile.org,uitest-privileged.gaiamobile.org,testpermission.gaiamobile.org,test-keyboard-app.gaiamobile.org,test-iac-publisher.gaiamobile.org,theme-test-2.gaiamobile.org,contacts-ds-provider2.gaiamobile.org,contacts-manager.gaiamobile.org,test-receiver-1.gaiamobile.org,l20n-app.gaiamobile.org,test-iac-subscriber.gaiamobile.org,test-otasp.gaiamobile.org,test-receiver-inline.gaiamobile.org,mochitest.gaiamobile.org,test-receiver-2.gaiamobile.org,test-sensors.gaiamobile.org,membuster.gaiamobile.org,test-startup-limit.gaiamobile.org,contacts-ds-provider1.gaiamobile.org,test-agent.gaiamobile.org,ds-test.gaiamobile.org,test-ime.gaiamobile.org,template.gaiamobile.org,marketplace-dev.allizom.org.gaiamobile.org,marketplace.allizom.org.gaiamobile.org,music-oga.gaiamobile.org,share-receiver.gaiamobile.org,geoloc.gaiamobile.org,uitest.gaiamobile.org,theme-test-3.gaiamobile.org,image-uploader.gaiamobile.org,test-wappush.gaiamobile.org,test-container.gaiamobile.org,fling-sender.gaiamobile.org,notification-sender.gaiamobile.org,remote-control.gaiamobile.org,dlna-player.gaiamobile.org,device-deck.gaiamobile.org,tv-epg.gaiamobile.org,browser.gaiamobile.org,tv-deck.gaiamobile.org,smart-home.gaiamobile.org,fling-player.gaiamobile.org,dashboard.gaiamobile.org,app-deck.gaiamobile.org,smart-system.gaiamobile.org,smart-settings.gaiamobile.org,remote-control-client.gaiamobile.org,weather-widget.gaiamobile.org,pocket.gaiamobile.org,notification-receiver.gaiamobile.org,fling-tutorial.gaiamobile.org");
+user_pref('devtools.responsiveUI.customWidth', 352);
+user_pref('devtools.responsiveUI.customHeight', 541);
+user_pref('devtools.responsiveUI.currentPreset', "custom");
+
+user_pref("devtools.debugger.prompt-connection", false);
+user_pref("devtools.debugger.forbid-certified-apps", false);
+user_pref("devtools.apps.forbidden-permissions", "");
+user_pref("b2g.software-buttons", true);
+
+// Required for Mulet in order to run the debugger server from the command line
+user_pref("devtools.debugger.remote-enabled", true);
+user_pref("devtools.chrome.enabled", true);
